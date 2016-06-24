abe06c75d6818382cc7cbffb312b053dca9cd134
Boris Kreminski <krembf@gmail.com>
2016-06-23 23:22:17 -0400

    LinqExample8 fix
    Removed ToList at the end which caused evaluation of the LINQ query and produced unexpected results.

--- LinqExercise_NoAnswers.cs
+++ LinqExercise_NoAnswers.cs
@@ -100,7 +100,7 @@ namespace CodeSmorgasbord
             //Eager Execution
             IList<int> numbers = new List<int>(new int[] { 1, 2 });
 
-            var powers = numbers.Select(n => n * n).ToList();
+            var powers = numbers.Select(n => n * n);
 
             numbers.Add(3);
 
